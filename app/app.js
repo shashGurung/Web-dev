db001bc45808a382f6d10cd677e0e86e119a0b29
Simon-E-H <149843814+Simon-E-H@users.noreply.github.com>
2023-12-07 16:38:03 +0000

    SignUp page has email sending functionaility

--- app/app.js
+++ app/app.js
@@ -1,7 +1,16 @@
 const express = require('express')
 const fs = require('fs')
+const nodemailer = require('nodemailer')
 const app = express();
 const port = 3000;
+
+var transporter = nodemailer.createTransport({
+    service: 'gmail',
+    auth: {
+        user: 'simonalt782@gmail.com',
+        pass: 'wnji ssjs uqhf howz'
+    }
+})
 
 const bodyParser = require('body-parser')
 const jsonParser = bodyParser.json();
@@ -22,10 +31,25 @@ app.post('/form', jsonParser, (req,res) => {
     const lastname = body.lastname;
     const email = body.email;
     const comments = body.comments;
-    var data = '\nEntry: First name: ' + firstname + ' , Last name: ' + lastname + ' , Email: ' +email+ ' , Comments: ' + comments
+    var data = '\nEntry: First name: ' + firstname + ' , Last name: ' + lastname + ' , Email: ' +email+ ' , Comments: ' + comments;
     fs.appendFile('formdata.txt', data, (err) =>{
         if (err) throw err;
-    })
+    });
+    var mailOptions = {
+        from: 'simonalt782@gmail.com',
+        to: email,
+        subject: 'Confirmation of Sign Up',
+        text: 'Thank you ' + firstname + ' ' + lastname + 'for signing up!\nWe will soon read your comment: "' + comments + '"'
+    };
+    transporter.sendMail(mailOptions, function(error, info){
+        if (error) {
+            console.log(error)
+
+        }
+        else{
+            console.log('Email sent: ' + info.response)
+        }
+    });
     res.send(' POST by form.js - first name = ' + firstname + ' , lastname = ' + lastname + ' , email = ' +email+ ' , comments = ' + comments);
     // Save this data to signups.txt
 })
