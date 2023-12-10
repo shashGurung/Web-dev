8b932eeae18edc63bcb3510902c7837c3c59b054
Simon-E-H <149843814+Simon-E-H@users.noreply.github.com>
2023-12-10 19:14:29 +0000

    Home page done

--- app/public/index.js
+++ app/public/index.js
@@ -8,16 +8,24 @@ const pageContent = {
     teamInfo: {
         title: '<a href="About-us.html">Team Information</a>',
         content: 'Our team is dedicated to spreading awareness and driving action towards Sustainable Development Goals 6, 9 and 10.'
-    }
+    },
+    pictures: [
+        {picture: 'Homepage-main.png'},
+        {picture: 'Homepage-goal6.png'},
+        {picture: 'homepage-goal9.png'},
+        {picture: 'Homepage-goal10.png'}
+    ]
 }
 
 document.addEventListener('DOMContentLoaded', function() {
     loadNavigation();
     loadTeamInfo();
+    loadImages();
     
     document.querySelector('.dropbtn').addEventListener('click', function() {
         this.querySelector('.arrow-down').classList.toggle('open');
     });
+    document.querySelector('#goallinks').addEventListener('click', clickHandler)
 });
 
 function loadNavigation() {
@@ -52,3 +60,31 @@ function loadTeamInfo() {
         <p>${pageContent.teamInfo.content}</p>
     `;
 }
+
+function loadImages() {
+    document.querySelector('#picture1').src = pageContent.pictures[0].picture
+    document.querySelector('#picture2').src = pageContent.pictures[1].picture
+    document.querySelector('#picture3').src = pageContent.pictures[2].picture
+    document.querySelector('#picture4').src = pageContent.pictures[3].picture
+}
+function clickHandler(e) {
+    console.log(e.currentTarget)
+    console.log(e.target)
+    if (e.currentTarget != e.target){
+        const targetID = e.target.id;
+        if (targetID == "picture2")
+        {
+            window.location = 'Goal-6.html'
+        }
+        else if (targetID == "picture3")
+        {
+            window.location = 'Goal-9.html'
+        }
+        else if(targetID == "picture4")
+        {
+            window.location = 'Goal-10.html'
+        }
+    }
+    
+}
+
